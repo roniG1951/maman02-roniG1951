0d83cc8c39de9367ab8aeeed5c298db8e4b1f7d5
baraksu <baraksu@gmail.com>
2026-01-13 23:16:32 +0200

    Sync HotelArielMeyer1Tester.java from maman02-ArielMeyer1

---
+++ HotelArielMeyer1Tester.java
@@ -0,0 +1,13 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class HotelArielMeyer1Tester {
+
+    @Test
+    public void test01() {
+
+    }
+
+}
+
