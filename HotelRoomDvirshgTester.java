87a7b898d6083af73665f009cf100c72c0298fbd
baraksu <baraksu@gmail.com>
2026-01-13 23:16:47 +0200

    Sync HotelRoomDvirshgTester.java from maman02-dvirshg

---
+++ HotelRoomDvirshgTester.java
@@ -0,0 +1,13 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class HotelRoomDvirshgTester {
+
+    @Test
+    public void test01() {
+
+    }
+
+}
+
