767d1ac6d983837007deda65f3b845fc36143f7c
roniG1951 <gotlibroni@gmail.com>
2026-02-03 16:30:49 +0200

    d2

--- Hotel.java
+++ Hotel.java
@@ -1,4 +1,13 @@
 
 public class Hotel {
-
+    public static void main(String [] args){
+        HotelRoom room1 = new HotelRoom(307, 4);
+        HotelRoom room2 = new HotelRoom(205, 3);
+        room2.checkIn("Test Guest");
+        HotelRoom room3 = new HotelRoom(402, 2);
+        System.out.println(room1);
+        System.out.println(room2);
+        System.out.println(room3);
+        System.out.println("Hotel Menu:\n1 -Display rooms by room number\n2 -Check-in to a room\n3 -Check-out from a room\n4 -Find available room by requested beds");
+    }
 }
